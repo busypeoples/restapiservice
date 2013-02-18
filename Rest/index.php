78a24c8e8b6030331dd40d54957ad38bbfa4a0b8
busypeoples <busypeoples@gmail.com>
2013-02-18 22:07:16 +0100

    minor reformatting

--- Rest/index.php
+++ Rest/index.php
@@ -6,23 +6,23 @@ $response = new \RestExample\Response();
 $response->addHeader('Content-Type', \RestExample\Helper\Converter::getContentType($request->getHttpAccept()));
 
 try {
-	$controller = \RestExample\Dispatcher::dispatch($request, $response);
-	$body = $controller->execute();
+    $controller = \RestExample\Dispatcher::dispatch($request, $response);
+    $body = $controller->execute();
         $status_code = \RestExample\Helper\Http::STATUS_CODE_200;
-	if ($request->getRequestMethod() == 'POST') {
-	    $status_code = \RestExample\Helper\Http::STATUS_CODE_201;
+    if ($request->getRequestMethod() == 'POST') {
+        $status_code = \RestExample\Helper\Http::STATUS_CODE_201;
             // add Location header...
             $response->addHeader('Location', 'newly created resouce uri');
         }
 } catch(\RestExample\Exception\ResourceNotFound $e) {
-	$status_code = \RestExample\Helper\Http::STATUS_CODE_404;
-	$body = null;        
+    $status_code = \RestExample\Helper\Http::STATUS_CODE_404;
+    $body = null;        
 } catch(\RestExample\Exception\RepresentationNotFound $e) {
-	$status_code = \RestExample\Helper\Http::STATUS_CODE_406;
-	$body = null;
+    $status_code = \RestExample\Helper\Http::STATUS_CODE_406;
+    $body = null;
 } catch(\Exception $e) {
-	$status_code = \RestExample\Helper\Http::STATUS_CODE_404;
-	$body = null;
+    $status_code = \RestExample\Helper\Http::STATUS_CODE_404;
+    $body = null;
 }
 
 // prepare the response and then execute...
