7966499321fae19f8ca0d3c441574c6d17e412c2
busypeoples <busypeoples@gmail.com>
2013-02-23 20:55:21 +0100

    Cleaned up index.php

--- Rest/index.php
+++ Rest/index.php
@@ -4,22 +4,3 @@ require_once('autoload.php');
 
 $service = new RestExample\ResourceService();
 $service->run();
-
-//try {
-//    $controller = \RestExample\ResourceService::dispatch($request, $response);
-//    $body = $controller->execute();
-//        $status_code = \RestExample\Response::STATUS_CODE_200;
-//    if ($request->getRequestMethod() == 'POST') {
-//        $status_code = \RestExample\Response::STATUS_CODE_201;
-//        // add Location header...
-//        $response->addHeader('Location', 'newly created resouce uri');
-//    }
-//} catch(\Exception $e) {
-//    $status_code = $e->getCode();
-//    $body = $e->getMessage(); 
-//}
-//
-//// prepare the response and then execute...
-//$response->setStatusCode($status_code);
-//$response->setBody($body);
-//$response->output();
