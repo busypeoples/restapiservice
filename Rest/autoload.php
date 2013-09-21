e9829a0023f2fda9dd0fc0ae94435a6ee228cbbe
busypeoples <busypeoples@gmail.com>
2013-09-21 23:01:12 +0200

    Updated autoload
    -- Skip if not inside RestExample namespace.

--- Rest/autoload.php
+++ Rest/autoload.php
@@ -5,6 +5,7 @@ define ('APPLICATION_PATH', __DIR__);
 
 // very basic autolaoder.
 function autoloading($class_name) {
+    if (strpos($class_name, 'RestExample') === false) return;
     $class = str_replace('\\', '/', $class_name);
     include APPLICATION_PATH . '/' . $class . '.php';
 }
