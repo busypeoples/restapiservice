a0c3092927dad5e12f0041e91b093c3fdbf3b852
busypeoples <busypeoples@gmail.com>
2013-09-21 23:45:26 +0200

    Added comment to autoload

--- Rest/autoload.php
+++ Rest/autoload.php
@@ -5,7 +5,9 @@ define ('APPLICATION_PATH', __DIR__);
 
 // very basic autolaoder.
 function autoloading($class_name) {
+    // skip if not inside RestExample namespace.
     if (strpos($class_name, 'RestExample') === false) return;
+    
     $class = str_replace('\\', '/', $class_name);
     include APPLICATION_PATH . '/' . $class . '.php';
 }
