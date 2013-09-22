646a184d5d401c6a71ba882f2971389739acb2c6
busypeoples <busypeoples@gmail.com>
2013-09-22 04:34:39 +0200

    Added Comments to Registry

--- Rest/RestExample/Registry.php
+++ Rest/RestExample/Registry.php
@@ -7,14 +7,25 @@ class Registry {
     
     private $collection;
     
+    /**
+     * prevent any new Operations
+     */
     private function __construct() {
         
     }
     
+    /**
+     * prevent any cloning
+     */
     private function __clone() {
         
     }
     
+    /**
+     * 
+     * @return Registry
+     * 
+     */
     public static function getInstance() {
         if (self::$registry === null) {
             self::$registry = new Registry();
@@ -23,10 +34,24 @@ class Registry {
         return self::$registry;
     } 
     
+    /**
+     * 
+     * @param mixed $key
+     * 
+     * @param mixed $value
+     * 
+     */
     public function set($key, $value) {
         $this->collection[$key] = $value;
     }
     
+    /**
+     * 
+     * @param mixed $key
+     * 
+     * @return boolean|mixed
+     * 
+     */
     public function get($key) {
         if (! $this->has($key)) {
             return false;
@@ -35,6 +60,13 @@ class Registry {
         return $this->collection[$key];
     }
     
+    /**
+     * 
+     * @param mixed $key
+     * 
+     * @return boolean
+     * 
+     */
     public function has($key) {
         return isset($key, $this->collection);
     }
